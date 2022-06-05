2bebb306e00518add50aa1df85871bdf7d6786f4
Reg Skelton <reg@davollo.com>
2022-06-05 18:24:22 +0100

    init 3

---
+++ sudoku.js
@@ -0,0 +1,278 @@
+var game = (function () {
+
+    var board;
+
+    class Cell {
+        constructor(size) {
+            this.done = false;
+            //digit = null;
+            this.candidates = new Set();
+
+            for (var d = 0; d < size; d++) {
+                this.candidates.add(d + 1);
+            }
+        }
+    }
+
+    class Board {
+        constructor(size) {
+            if (size < 1) {
+                console.log("Error, grid must be at least 1x1");
+            }
+
+            this.size = size;
+
+            this.cells = new Array(size);
+
+            for (var r = 0; r < size; r++) {
+                this.cells[r] = new Array(size);
+
+                for (var c = 0; c < size; c++) {
+                    this.cells[r][c] = new Cell(size);
+                }
+            }
+
+            this.setDigit = function (r, c, d) {
+                if (this.cells[r][c].done) {
+                    console.log("Cell[" + (r + 1) + "][" + (c + 1) + "]" + this.cells[r][c].digit + "<-" + d);
+                } else {
+                    console.log("Cell[" + (r + 1) + "][" + (c + 1) + "]=" + d);
+                }
+
+                this.cells[r][c].digit = d;
+                this.cells[r][c].candidates.clear();
+                this.cells[r][c].done = true;
+            };
+
+            this.eliminate = function (y, x, digit) {
+                var changed = false;
+
+                for (var r = 0; r < this.size; r++) {
+                    for (var c = 0; c < this.size; c++) {
+                        // Are we looking at (same row, but different column) or (different row, but same column) ?
+                        // ...or same box?
+                        //Same box if when row,col divide by 3 (integer divide) is same as the source cell
+                        if (
+                            (r == y && c != x) ||
+                            (r != y && c == x)
+                            || (Math.floor(r / 3) == Math.floor(y / 3) && Math.floor(c / 3) == Math.floor(x / 3))
+                        ) {
+                            if (this.cells[r][c].candidates.has(digit)) {
+
+                                if (r == 8 && c == 0) {
+                                    // console.log(`Elim ${digit} from [${r + 1},${c + 1}] source[${y + 1},${x + 1}]`);
+                                }
+                                this.cells[r][c].candidates.delete(digit);
+
+                                changed = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            /*
+                          1|29e cec e15 
+            sudoku.js:116 2|8dc 172 dd6 
+            sudoku.js:116 3|eff dgd gef 
+            sudoku.js:119   --- --- --- 
+            sudoku.js:116 4|1ef eed ggd 
+            sudoku.js:116 5|c6d 9d4 e8c 
+            sudoku.js:116 6|dc8 ce5 g7e 
+            sudoku.js:119   --- --- --- 
+            sudoku.js:116 7|e2f de8 gee 
+            sudoku.js:116 8|cd5 6dd gd3 
+            sudoku.js:116 9|7de 4fd gee 
+*/
+            this.eliminateAll = function () {
+                var changed = false;
+
+                for (var r = 0; r < this.size; r++) {
+                    for (var c = 0; c < this.size; c++) {
+                        if (!this.cells[r][c].done) {
+                            if (this.cells[r][c].candidates.size == 1) {
+                                this.setDigit(r, c, [...this.cells[r][c].candidates][0]);
+
+                                changed = true;
+                            }
+                        }
+
+                        if (this.cells[r][c].done) {
+                            if (this.eliminate(r, c, this.cells[r][c].digit)) {
+                                changed = true;
+                            }
+                        }
+                    }
+
+                }
+
+                return changed;
+            }
+
+            this.log = function () {
+                console.log("  123 456 789");
+                console.log("  --- --- ---");
+                for (var r = 0; r < size; r++) {
+                    var text = "" + (r + 1) + "|";
+
+                    for (var c = 0; c < size; c++) {
+                        if (this.cells[r][c].done) {
+                            text += this.cells[r][c].digit;
+                        } else {
+                            text += String.fromCharCode(96 + this.cells[r][c].candidates.size);
+                        }
+
+                        if (c % 3 == 2) {
+                            text += " ";
+                        }
+                    }
+
+                    text += " ";
+
+                    for (var c = 0; c < size; c++) {
+                        if (this.cells[r][c].done) {
+                            text += " xxx";
+                        } else {
+                            text += " " + this.cells[r][c].candidates.size + ":";
+
+                            this.cells[r][c].candidates.forEach((item, index, set) => {
+                                text += item;
+                            });
+                        }
+                    }
+
+                    console.log(text);
+
+                    if (r % 3 == 2) {
+                        console.log("  --- --- --- ");
+                    }
+                }
+            }
+        }
+    }
+
+
+    return {
+        newGame: function (size) {
+            board = new Board(size);
+        },
+
+        play: function () {
+        },
+
+        getBoard: function () {
+            return board;
+        }
+    }
+})();
+
+var uiController = (function () {
+
+})();
+
+var boardTests = [
+    {
+        "in": [
+            [2, 9, 0, 0, 0, 0, 0, 1, 5],
+            [8, 0, 0, 1, 7, 2, 0, 0, 6],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 6, 0, 9, 0, 4, 0, 8, 0],
+            [0, 0, 8, 0, 0, 5, 0, 7, 0],
+            [0, 2, 0, 0, 0, 8, 0, 0, 0],
+            [0, 0, 5, 6, 0, 0, 0, 0, 3],
+            [7, 0, 0, 4, 0, 0, 0, 0, 0]
+        ]
+    },
+    {
+        "in": [
+            [3, 9, 0, 0, 0, 2, 0, 0, 0],
+            [0, 0, 7, 0, 0, 4, 8, 0, 0],
+            [0, 0, 4, 0, 5, 0, 1, 9, 6],
+            [6, 7, 2, 1, 0, 0, 0, 8, 4],
+            [0, 3, 1, 9, 4, 0, 0, 0, 0],
+            [0, 4, 0, 7, 0, 0, 6, 1, 0],
+            [9, 2, 0, 4, 0, 3, 5, 6, 0],
+            [0, 0, 3, 5, 0, 1, 4, 0, 9],
+            [0, 0, 0, 0, 6, 9, 0, 0, 0],
+        ]
+    },
+];
+
+
+
+
+var controller = (function (game, UICtrl) {
+
+
+    var play = function () {
+        game.newGame(4);
+
+        var moves = 0;
+
+        game.play();
+
+        while (true) {
+            game.play();
+        }
+    }
+
+    var runTests = function () {
+        var tests = boardTests;
+
+        for (t = 0; t < tests.length; t++) {
+            console.log("Running test " + t);
+
+            var size = tests[t].in[0].length;
+
+            game.newGame(size);
+
+            var board = game.getBoard();
+
+            for (r = 0; r < size; r++) {
+                for (c = 0; c < size; c++) {
+                    if (tests[t].in[r][c] != 0) {
+                        board.setDigit(r, c, tests[t].in[r][c]);
+                    }
+                }
+            }
+
+            board.log();
+
+            for (var c = 0; c < 5000; c++) {
+                board.eliminateAll();
+            }
+
+            board.log();
+
+            /*            
+                        moved = game.moveLeft();;
+            
+                        for (r = 0; r < size; r++) {
+                            for (c = 0; c < size; c++) {
+                                if (cells[r][c] != tests[t].out[r][c]) {
+                                    console.log("Test" + t + "(" + r + "," + c + ")=" + cells[r][c] + ", not " + tests[t].out[r][c] + ".");
+                                } else {
+                                    // console.log("Test" + t + "(" + r + "," + c + ")=" + cells[r][c] + ", yes " + tests[t].out[r][c] + ".");
+                                }
+                            }
+                        }
+            */
+        }
+    };
+
+    return {
+        init: function () {
+            console.log('Regoku init');
+
+            runTests();
+
+            //play();
+
+            console.log("Jump to hyperspace...");
+        }
+    };
+})(game, uiController);
+
+controller.init();
